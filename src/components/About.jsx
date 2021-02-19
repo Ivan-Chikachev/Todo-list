8fb3f57f456221a7284d7a8b8302613b668238ec
Ivan Ch <ivan-chikachev@mail.ru>
2021-02-19 12:05:15 +0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+
+const Page = (props) => {
+    const [type, setType] = useState('Name')
+
+    return (
+
+        <>
+            <h1>SAs</h1>
+        </>
+    )
+}
+export default Page;
